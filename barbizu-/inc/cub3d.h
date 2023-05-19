a5a686e0fa25928da5f52163d5eb59f89694e2de
Belen Arbizu jimenez <barbizu-@c3r6s4.42malaga.com>
2023-05-19 10:30:07 +0200

    1905

---
+++ barbizu-/inc/cub3d.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aruiz-mo <aruiz-mo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 19:11:15 by aruiz-mo          #+#    #+#             */
+/*   Updated: 2023/05/19 09:40:44 by aruiz-mo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+# include <stdio.h>
+# include <stdlib.h>
+# include "../libs/libft/libft.h"
+# include "../libs/MLX42/include/MLX42/MLX42.h"
+
+#endif
