befd1f06e6925c263e58504699631a5d5af4ef44
Antonio jesus Ruiz moreno <aruiz-mo@c3r5s2.42malaga.com>
2023-05-26 11:26:36 +0200

    2605

--- aruiz-mo/src/main.c
+++ aruiz-mo/src/main.c
@@ -6,61 +6,118 @@
 /*   By: aruiz-mo <aruiz-mo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 18:53:19 by aruiz-mo          #+#    #+#             */
-/*   Updated: 2023/05/24 09:34:14 by aruiz-mo         ###   ########.fr       */
+/*   Updated: 2023/05/26 10:35:22 by aruiz-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
-t_game	*init_game(t_game *game)
+t_dates	*init_dates(t_dates *dates)
 {
-	game->no = (char *) malloc(sizeof(char));
-	game->no[0] = '\0';
-	game->so = (char *) malloc(sizeof(char));
-	game->so[0] = '\0';
-	game->we = (char *) malloc(sizeof(char));
-	game->we[0] = '\0';
-	game->ea = (char *) malloc(sizeof(char));
-	game->ea[0] = '\0';
-	game->c = (char **) malloc(sizeof(char *));
-	game->c[0] = NULL;
-	game->f = (char **) malloc(sizeof(char *));
-	game->f[0] = NULL;
-	game->map = (char **) malloc(sizeof(char *));
-	game->map[0] = NULL;
-	return (game);
+	dates->no = (char *) malloc(sizeof(char));
+	dates->no[0] = '\0';
+	dates->so = (char *) malloc(sizeof(char));
+	dates->so[0] = '\0';
+	dates->we = (char *) malloc(sizeof(char));
+	dates->we[0] = '\0';
+	dates->ea = (char *) malloc(sizeof(char));
+	dates->ea[0] = '\0';
+	dates->c = (char **) malloc(sizeof(char *));
+	dates->c[0] = NULL;
+	dates->f = (char **) malloc(sizeof(char *));
+	dates->f[0] = NULL;
+	dates->map = (char **) malloc(sizeof(char *));
+	dates->map[0] = NULL;
+	return (dates);
 }
 
-t_game	*free_game(t_game *game)
+t_dates	*free_dates(t_dates *dates)
 {
-	free (game->no);
-	free (game->so);
-	free (game->we);
-	free (game->ea);
-	ft_free_table(game->c);
-	free(game->c);
-	ft_free_table(game->f);
-	free(game->f);
-	ft_free_table(game->map);
-	free(game->map);
-	return (game);
+	free (dates->no);
+	free (dates->so);
+	free (dates->we);
+	free (dates->ea);
+	ft_free_table(dates->c);
+	free(dates->c);
+	ft_free_table(dates->f);
+	free(dates->f);
+	ft_free_table(dates->map);
+	free(dates->map);
+	return (dates);
+}
+
+void	put_map(char **map, t_game *game)
+{
+	int			i;
+	int			j;
+	mlx_image_t	*img;
+	uint32_t	x;
+	uint32_t	y;
+
+	i = -1;
+	while (map[++i])
+	{
+		j = -1;
+		while (map[i][++j])
+		{
+			mlx_image_to_window(game->mlx, game->img, j * 30, i * 30);
+			if (map[i][j] == '1')
+			{
+				printf("%c", map[i][j]);
+				img = mlx_new_image(game->mlx, 30, 30);
+				if (!img
+					|| (mlx_image_to_window(game->mlx, img, j * 30, i * 30)
+						< 0))
+					exit(EXIT_FAILURE);
+				y = 0;
+				while (y < img->height)
+				{
+					x = 0;
+					while (x < img->width)
+					{
+						mlx_put_pixel(img, x, y, 0xFF0000FF);
+						x++;
+					}
+					y++;
+				}
+				mlx_image_to_window(game->mlx, img, j * 30, i * 30);
+			}
+		}
+	}
+}
+
+static void	ft_error(void)
+{
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
 }
 
 int	main(int argc, char **argv)
 {
+	t_dates	*dates;
 	t_game	*game;
 
+	dates = malloc(sizeof(t_dates));
+	if (!dates)
+		return (EXIT_SUCCESS);
+	dates = init_dates(dates);
+	if (argv_validate(argc, argv, &dates) == 0)
+	{
+		dates = free_dates(dates);
+		free(dates);
+		return (EXIT_SUCCESS);
+	}
 	game = malloc(sizeof(t_game));
 	if (!game)
 		return (EXIT_SUCCESS);
-	game = init_game(game);
-	if (argv_validate(argc, argv, &game) == 0)
-	{
-		game = free_game(game);
-		free(game);
-		return (EXIT_SUCCESS);
-	}
-	game = free_game(game);
-	free(game);
+	game->mlx = mlx_init(WIDTH, HEIGHT, "Cub3D", true);
+	if (!game->mlx)
+		ft_error();
+	game->img = mlx_new_image(game->mlx, 800, 600);
+	put_map(dates->map, game);
+	mlx_loop(game->mlx);
+	mlx_terminate(game->mlx);
+	dates = free_dates(dates);
+	free(dates);
 	return (EXIT_SUCCESS);
 }
