b3ef8becf9719b32c6282914fd82763f47358de2
Antonio jesus Ruiz moreno <aruiz-mo@c3r9s5.42malaga.com>
2023-06-13 13:51:22 +0200

    1306 1400

--- cub3d/inc/cub3d.h
+++ cub3d/inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: aruiz-mo <aruiz-mo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 19:11:15 by aruiz-mo          #+#    #+#             */
-/*   Updated: 2023/06/09 13:55:58 by aruiz-mo         ###   ########.fr       */
+/*   Updated: 2023/06/13 13:43:39 by aruiz-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,61 +51,81 @@ typedef struct s_player
 
 typedef struct s_game
 {
-	int			time;
-	int			old_time;
 	mlx_t		*mlx;
-	mlx_image_t	*img;
 	t_data		*data;
 	t_player	*player;
 }	t_game;
 
+typedef struct s_ray
+{
+	mlx_image_t	*img;
+	uint32_t	color;
+	int			x;
+	int			y_init;
+	int			y_end;
+}	t_ray;
+
 //functions argv_validate
 
-int		argv_validate(int argc, char **argv, t_data **data);
-int		read_file(char *file_name, t_data **data);
-int		check_file(char *file_name);
-int		parse_file(int fd, t_data **data, char *line);
-char	*parse_file_aux(int fd, t_data **data, char *line, int ctrl);
+int			argv_validate(int argc, char **argv, t_data **data);
+int			read_file(char *file_name, t_data **data);
+int			check_file(char *file_name);
+int			parse_file(int fd, t_data **data, char *line);
+char		*parse_file_aux(int fd, t_data **data, char *line, int ctrl);
 
 //functions color_validate
 
-int		put_color(char ***tex, char *line);
-int		rgb_text(char **tex);
-int		init_color(char ***tex, char *line, int i);
-int		test_number(char *line, int i);
-void	set_color(char ***tex, char *line, int i);
+int			put_color(char ***tex, char *line);
+int			rgb_text(char **tex);
+int			init_color(char ***tex, char *line, int i);
+int			test_number(char *line, int i);
+void		set_color(char ***tex, char *line, int i);
 
 //functions texture_validate
 
-int		texture_data(char *line, t_data **data, int *ctrl_data, int *ini_map);
-int		set_texture(t_data **data, int **ctrl_data, char *line, int i);
-int		put_texture(char **str, char *line);
-int		is_map(char *line, int *ctrl_data, int *ini_map);
+int			texture_data(char *line, t_data **data,
+				int *ctrl_data, int *ini_map);
+int			set_texture(t_data **data, int **ctrl_data, char *line, int i);
+int			put_texture(char **str, char *line);
+int			is_map(char *line, int *ctrl_data, int *ini_map);
 
 //functions read_map
 
-int		init_map(int *ctrl_data, int *ini_map);
-void	read_map(char *line, char ***map, int *ini_map);
-void	add_line(char ***map, char *line, int nl);
+int			init_map(int *ctrl_data, int *ini_map);
+void		read_map(char *line, char ***map, int *ini_map);
+void		add_line(char ***map, char *line, int nl);
 
 //functions map_validate
 
-int		map_validate(char **map, int ini_map, int *mf, int *mc);
-int		text_border(char l);
-int		text_int(char l, int i, int j, char **map);
-char	**norm_map(char **map, int *mf, int *mc);
-int		player_exist(char **map);
+int			map_validate(char **map, int ini_map, int *mf, int *mc);
+int			text_border(char l);
+int			text_int(char l, int i, int j, char **map);
+char		**norm_map(char **map, int *mf, int *mc);
+int			player_exist(char **map);
 
 //functions char_array_utils
 
-int		ft_get_len(char **array);
-char	**ft_copy(char **dest, char **src);
-char	**ft_free_table(char **array);
-char	**duplicate_array(char **map);
-char	**ft_add_item(char **array, char *item);
+int			ft_get_len(char **array);
+char		**ft_copy(char **dest, char **src);
+char		**ft_free_table(char **array);
+char		**duplicate_array(char **map);
+char		**ft_add_item(char **array, char *item);
 
-//functions wall_operation
+//functions cub3d_utils
 
-double 	calc_rad(double g);
+uint32_t	get_rgba(char **rgb);
+double		calc_rad(double g);
+int			is_wall(int x, int y, char **map);
+t_data		*free_data(t_data *data);
+void		draw_line(t_game *game, t_ray ray);
+
+//functions init_cub3d
+t_data		*init_data(t_data *data);
+t_game		*init_game(t_data **data, t_player **player, t_game *game);
+t_player	*init_player(t_data **data, t_player *player);
+t_player	*init_player_aux(t_data **data, t_player *player, int i, int j);
+
+//functions main
+void		print_map(t_game *game);
 
 #endif
--- cub3d/src/main.c
+++ cub3d/src/main.c
@@ -6,133 +6,15 @@
 /*   By: aruiz-mo <aruiz-mo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 18:53:19 by aruiz-mo          #+#    #+#             */
-/*   Updated: 2023/06/10 15:16:03 by aruiz-mo         ###   ########.fr       */
+/*   Updated: 2023/06/13 13:49:37 by aruiz-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
-t_data	*init_data(t_data *data)
-{
-	data->no = (char *) malloc(sizeof(char));
-	data->no[0] = '\0';
-	data->so = (char *) malloc(sizeof(char));
-	data->so[0] = '\0';
-	data->we = (char *) malloc(sizeof(char));
-	data->we[0] = '\0';
-	data->ea = (char *) malloc(sizeof(char));
-	data->ea[0] = '\0';
-	data->c = (char **) malloc(sizeof(char *));
-	data->c[0] = NULL;
-	data->f = (char **) malloc(sizeof(char *));
-	data->f[0] = NULL;
-	data->map = (char **) malloc(sizeof(char *));
-	data->map[0] = NULL;
-	return (data);
-}
-
-t_data	*free_data(t_data *data)
-{
-	free (data->no);
-	free (data->so);
-	free (data->we);
-	free (data->ea);
-	ft_free_table(data->c);
-	free(data->c);
-	ft_free_table(data->f);
-	free(data->f);
-	ft_free_table(data->map);
-	free(data->map);
-	return (data);
-}
-
-t_player	*init_player(t_data **data, t_player *player)
-{
-	int				ctrl;
-	int				i;
-	int				j;
-
-	i = -1;
-	ctrl = 1;
-	while (ctrl == 1 && (*data)->map[++i])
-	{
-		j = -1;
-		while (ctrl == 1 && (*data)->map[i][++j])
-		{
-			if ((*data)->map[i][j] == 'N' || (*data)->map[i][j] == 'S'
-				|| (*data)->map[i][j] == 'W' || (*data)->map[i][j] == 'E')
-					ctrl = 0;
-		}
-	}
-	player->pos.y = i;
-	player->pos.x = j;
-	player->turn = calc_rad(2);
-	if ((*data)->map[i][j] == 'N')
-	{
-		player->dir.x = 0;
-		player->dir.y = -1;
-		player->plane.x = 1;
-		player->plane.y = 0;
-	}
-	else if ((*data)->map[i][j] == 'E')
-	{
-		player->dir.x = 1;
-		player->dir.y = 0;
-		player->plane.x = 0;
-		player->plane.y = -1;
-	}
-	else if ((*data)->map[i][j] == 'S')
-	{
-		player->dir.x = 0;
-		player->dir.y = 1;
-		player->plane.x = -1;
-		player->plane.y = 0;
-	}
-	else if ((*data)->map[i][j] == 'W')
-	{
-		player->dir.x = -1;
-		player->dir.y = 0;
-		player->plane.x = 0;
-		player->plane.y = 1;
-	}
-	(*data)->map[i][j] = '0';
-	return (player);
-}
-uint32_t get_rgba(char **rgb)
-{
-	int		r;
-	int		g;
-	int		b;
-
-	r = ft_atoi(rgb[0]);
-	g = ft_atoi(rgb[1]);
-	b = ft_atoi(rgb[2]);
-    return (r << 24 | g << 16 | b << 8 | 255);
-}
-void draw_line(t_game *game, int x, int y_init, int y_end, uint32_t color, mlx_image_t	*img)
-{
-	int	i;
-
-	i = 0;
-	while (i < y_init)
-	{
-		mlx_put_pixel(img, x, i, get_rgba(game->data->c));
-		i++;
-	}
-	while (i < y_end)
-	{
-		mlx_put_pixel(img, x, i, color);
-		i++;
-	}
-	while (i < HEIGHT)
-	{
-		mlx_put_pixel(img, x, i, get_rgba(game->data->f));
-		i++;
-	}
-}
 void print_map(t_game *game)
 {
-	int		x;
+	t_ray	ray;
 	int		map_x;
 	int		map_y;
 	double	camera_x;
@@ -148,32 +30,25 @@ void print_map(t_game *game)
     int		side;
 	int		line_h;
 	double	perp_wall_dist;
-	int		draw_start;
-	int		draw_end;
-	uint32_t	color;
 
-	x = 0;
-	mlx_image_t	*img;
-
-	img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
-	if (!img)
+	ray.img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+	if (!ray.img || mlx_image_to_window(game->mlx, ray.img, 0, 0) < 0)
 		printf("ERROR\n");
-	if (mlx_image_to_window(game->mlx, img, 0, 0) < 0)
-		printf("ERROR\n");
-	while(x < WIDTH)
-    {
+	ray.x = 0;
+	while (ray.x < WIDTH)
+	{
 		hit = 0;
-		delta_x = pow(10,30);
-		delta_y = pow(10,30);
+		delta_x = pow(10, 30);
+		delta_y = pow(10, 30);
 		map_x = (int)game->player->pos.x;
 		map_y = (int)game->player->pos.y;
-		camera_x = 2 * x / (double)WIDTH - 1;
+		camera_x = 2 * ray.x / (double)WIDTH - 1;
 		ray_x = game->player->dir.x + game->player->plane.x * camera_x;
 		ray_y = game->player->dir.y + game->player->plane.y * camera_x;
 		if (ray_x != 0)
-			delta_x = fabs(1/ray_x);
+			delta_x = fabs(1 / ray_x);
 		if (ray_y != 0)
-			delta_y = fabs(1/ray_y);
+			delta_y = fabs(1 / ray_y);
 		if (ray_x < 0)
 		{
 			step_x = -1;
@@ -208,34 +83,36 @@ void print_map(t_game *game)
 				map_y += step_y;
 				side = 1;
 			}
-			if (game->data->map[map_y][map_x] != '0') 
+			if (game->data->map[map_y][map_x] != '0')
 				hit = 1;
 		}
-		if(side == 0)
+		if (side == 0)
 		{
 			perp_wall_dist = (side_dist_x - delta_x);
-			color = 0x0000FFFF;
+			ray.color = 0x0000FFFF;
 		}
 		else
 		{
 			perp_wall_dist = (side_dist_y - delta_y);
-			color = 0x000000FF;
+			ray.color = 0x000000FF;
 		}
 		line_h = (int)(HEIGHT / perp_wall_dist);
-		draw_start = -line_h / 2 + HEIGHT / 2;
-		if (draw_start < 0)
-			draw_start = 0;
-		draw_end = line_h / 2 + HEIGHT / 2;
-		if(draw_end >= HEIGHT)
-			draw_end = HEIGHT - 1;
-		draw_line(game, x, draw_start, draw_end, color, img);
-		x++;
+		ray.y_init = -line_h / 2 + HEIGHT / 2;
+		if (ray.y_init < 0)
+			ray.y_init = 0;
+		ray.y_end = line_h / 2 + HEIGHT / 2;
+		if (ray.y_end >= HEIGHT)
+			ray.y_end = HEIGHT - 1;
+		draw_line(game, ray);
+		ray.x++;
 	}
 }
 
 void	hook(mlx_key_data_t keydata, t_game **game)
 {
 	double	oldx;
+	double	newx;
+	double	newy;
 
 	if (keydata.key == MLX_KEY_RIGHT && (keydata.action == MLX_PRESS
 			|| keydata.action == MLX_REPEAT))
@@ -265,30 +142,50 @@ void	hook(mlx_key_data_t keydata, t_game **game)
 	if (keydata.key == MLX_KEY_W && (keydata.action == MLX_PRESS
 			|| keydata.action == MLX_REPEAT))
 	{
-		(*game)->player->pos.x += (*game)->player->dir.x * SPEED;
-		(*game)->player->pos.y += (*game)->player->dir.y * SPEED;
-		print_map(*game);
+		newx = (*game)->player->pos.x + (*game)->player->dir.x * SPEED;
+		newy = (*game)->player->pos.y + (*game)->player->dir.y * SPEED;
+		if (!is_wall((int)newx, (int)newy, (*game)->data->map))
+		{
+			(*game)->player->pos.x = newx;
+			(*game)->player->pos.y = newy;
+			print_map(*game);
+		}
 	}
 	if (keydata.key == MLX_KEY_S && (keydata.action == MLX_PRESS
 			|| keydata.action == MLX_REPEAT))
 	{
-		(*game)->player->pos.x -= (*game)->player->dir.x * SPEED;
-		(*game)->player->pos.y -= (*game)->player->dir.y * SPEED;
-		print_map(*game);
+		newx = (*game)->player->pos.x - (*game)->player->dir.x * SPEED;
+		newy = (*game)->player->pos.y - (*game)->player->dir.y * SPEED;
+		if (!is_wall((int)newx, (int)newy, (*game)->data->map))
+		{
+			(*game)->player->pos.x = newx;
+			(*game)->player->pos.y = newy;
+			print_map(*game);
+		}
 	}
 	if (keydata.key == MLX_KEY_A && (keydata.action == MLX_PRESS
 			|| keydata.action == MLX_REPEAT))
 	{
-		(*game)->player->pos.x += (*game)->player->dir.y * SPEED;
-		(*game)->player->pos.y -= (*game)->player->dir.x * SPEED;
-		print_map(*game);
+		newx = (*game)->player->pos.x + (*game)->player->dir.y * SPEED;
+		newy = (*game)->player->pos.y - (*game)->player->dir.x * SPEED;
+		if (!is_wall((int)newx, (int)newy, (*game)->data->map))
+		{
+			(*game)->player->pos.x = newx;
+			(*game)->player->pos.y = newy;
+			print_map(*game);
+		}
 	}
 	if (keydata.key == MLX_KEY_D && (keydata.action == MLX_PRESS
 			|| keydata.action == MLX_REPEAT))
 	{
-		(*game)->player->pos.x -= (*game)->player->dir.y * SPEED;
-		(*game)->player->pos.y += (*game)->player->dir.x * SPEED;
-		print_map(*game);
+		newx = (*game)->player->pos.x - (*game)->player->dir.y * SPEED;
+		newy = (*game)->player->pos.y + (*game)->player->dir.x * SPEED;
+		if (!is_wall((int)newx, (int)newy, (*game)->data->map))
+		{
+			(*game)->player->pos.x = newx;
+			(*game)->player->pos.y = newy;
+			print_map(*game);
+		}
 	}
 }
 
@@ -299,7 +196,9 @@ int	main(int argc, char **argv)
 	t_player	*player;
 
 	data = malloc(sizeof(t_data));
-	if (!data)
+	game = malloc(sizeof(t_game));
+	player = malloc(sizeof(t_player));
+	if (!data || !game || !player)
 		return (EXIT_SUCCESS);
 	data = init_data(data);
 	if (argv_validate(argc, argv, &data) == 0)
@@ -308,22 +207,7 @@ int	main(int argc, char **argv)
 		free(data);
 		return (EXIT_SUCCESS);
 	}
-	game = malloc(sizeof(t_game));
-	if (!game)
-		return (EXIT_SUCCESS);
-	player = malloc(sizeof(t_player));
-	if (!player)
-		return (EXIT_SUCCESS);
-	game->mlx = mlx_init(WIDTH, HEIGHT, "Cub3D", true);
-	if (!game->mlx)
-		return (EXIT_SUCCESS);
-	player = init_player(&data, player);
-	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT); //???
-	game->player = player;
-	game->data = data;
-	game->time = 0;
-	game->old_time = 0;
-	print_map(game);
+	game = init_game(&data, &player, game);
 	mlx_key_hook(game->mlx, (mlx_keyfunc)hook, &game);
 	mlx_loop(game->mlx);
 	mlx_terminate(game->mlx);
